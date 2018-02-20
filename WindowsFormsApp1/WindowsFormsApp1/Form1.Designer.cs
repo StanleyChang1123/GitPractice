558811d46811b5b1222ef0f43c33810bf398a76a
Stanley's mom's pc <koala.cyc@gmail.com>
2018-02-20 13:48:15 +0800

    Whole solution for E-Learning

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 75);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(752, 302);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Encrypt and decrypt file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_genEnDecryptFile);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(226, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Play Encrypt File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(375, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Play Decrypt File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 389);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+            //Stanley add
+            //try
+            //{
+
+            //}
+            //catch (Exception ee)
+            //{
+            //    var a = ee;
+            //    var b = 1;
+            ////}
+            //if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            //{
+            //    axWindowsMediaPlayer1.fullScreen = true;
+            //}
+
+            //axWindowsMediaPlayer1.URL = @"C:\Users\iphon\Desktop\Gmail\Hu.mp4";
+       
+        }
+
+        private byte[] getOriginalFileStream(string filePath)
+        {
+            long fileLength = 0;
+            byte[] originalByteArray;
+
+            using (FileStream fsSource = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                fileLength = fsSource.Length;
+                originalByteArray = new byte[fileLength];
+
+                int numBytesToRead = (int)fsSource.Length;
+                int numBytesRead = 0;
+                while (numBytesToRead > 0)
+                {
+                    int n = fsSource.Read(originalByteArray, numBytesRead, numBytesToRead);    // Read may return anything from 0 to numBytesToRead.
+                    if (n == 0)  // Break when the end of the file is reached.
+                        break;
+                    numBytesRead += n;
+                    numBytesToRead -= n;
+                }
+                numBytesToRead = originalByteArray.Length;
+            }
+
+            return originalByteArray;
+        }
+
+        private byte[] writeEncryptFile(byte[] source, string pathEncrypt)
+        {
+            #region 加解密 key
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] key = Encoding.ASCII.GetBytes("12345678");
+            byte[] iv = Encoding.ASCII.GetBytes("87654321");
+            des.Key = key;
+            des.IV = iv;
+            #endregion 加解密 key
+
+            #region 加密
+            int fileLength = source.Length;
+
+            string encrypt = "";
+            byte[] encryptByteArray = new byte[fileLength];
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(source, 0, source.Length);
+                    cs.FlushFinalBlock();
+                    encrypt = Convert.ToBase64String(ms.ToArray());
+                    encryptByteArray = Convert.FromBase64String(encrypt);
+                }
+            }
+
+            using (FileStream fsNew = new FileStream(pathEncrypt,
+                    FileMode.Create, FileAccess.Write))
+            {
+                fsNew.Write(encryptByteArray, 0, (int)fileLength);
+            }
+            #endregion 加密
+            return encryptByteArray;
+        }
+
+        private void writeDecryptFile(byte[] encrypt, int originalFileLength, string pathDecrypt)
+        {
+            #region 加解密 key
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] key = Encoding.ASCII.GetBytes("12345678");
+            byte[] iv = Encoding.ASCII.GetBytes("87654321");
+            des.Key = key;
+            des.IV = iv;
+            #endregion 加解密 key
+
+            //#region 解密
+            byte[] decryptByteArray = new byte[originalFileLength];
+            des.Key = Encoding.ASCII.GetBytes("12345678");
+            des.IV = Encoding.ASCII.GetBytes("87654321");
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(encrypt, 0, encrypt.Length);
+                    cs.FlushFinalBlock();
+                    decryptByteArray = ms.ToArray();
+                }
+            }
+
+            using (FileStream fsNew = new FileStream(pathDecrypt,
+                FileMode.Create, FileAccess.Write))
+            {
+                fsNew.Write(decryptByteArray, 0, (int)originalFileLength);
+            }
+        }
+
+        private void button_genEnDecryptFile(object sender, EventArgs e)
+        {
+            string pathSource = @"C:\Testing\LearningSystem\Original.mp4";
+            byte[] originalByteArray = getOriginalFileStream(pathSource);
+
+            string pathEncrypt = @"C:\Testing\LearningSystem\Encrypt.mp4";
+            var encryptByteArray = writeEncryptFile(originalByteArray, pathEncrypt);
+            string pathDecrypt = @"C:\Testing\LearningSystem\Decrypt.mp4";
+
+            writeDecryptFile(encryptByteArray, originalByteArray.Length, pathDecrypt);
+
+            //long fileLength = originalByteArray.Length;
+          
+
+
+          
+
+            //#endregion 解密
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = @"C:\Testing\LearningSystem\Encrypt.mp4";
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = @"C:\Testing\LearningSystem\Decrypt.mp4";
+        }
+    }
+}
