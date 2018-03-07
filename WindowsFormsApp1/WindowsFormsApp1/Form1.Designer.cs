de6a58ca53ddfac6cc6b5cc28516c518070a792b
Stanley's mom's pc <koala.cyc@gmail.com>
2018-03-08 02:30:00 +0800

    File path in common

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -31,14 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             this.tB_FileName = new System.Windows.Forms.TextBox();
-            this.label1Example = new System.Windows.Forms.Label();
-            this.tb_TextSourceExample = new System.Windows.Forms.TextBox();
-            this.button_EncryptTextExample = new System.Windows.Forms.Button();
-            this.tB_EncryptExample = new System.Windows.Forms.TextBox();
-            this.button_DecryptTextExample = new System.Windows.Forms.Button();
-            this.tB_DecryptExample = new System.Windows.Forms.TextBox();
             this.button_EncryptFile = new System.Windows.Forms.Button();
-            this.button_DecryptFileExample = new System.Windows.Forms.Button();
             this.button_DecryptAndPlay = new System.Windows.Forms.Button();
             this.comboBoxForCourse = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
@@ -56,81 +49,20 @@
             // 
             // tB_FileName
             // 
-            this.tB_FileName.Location = new System.Drawing.Point(646, 75);
+            this.tB_FileName.Location = new System.Drawing.Point(351, 38);
             this.tB_FileName.Name = "tB_FileName";
             this.tB_FileName.Size = new System.Drawing.Size(100, 22);
             this.tB_FileName.TabIndex = 4;
             // 
-            // label1Example
-            // 
-            this.label1Example.AutoSize = true;
-            this.label1Example.Location = new System.Drawing.Point(603, 213);
-            this.label1Example.Name = "label1Example";
-            this.label1Example.Size = new System.Drawing.Size(37, 12);
-            this.label1Example.TabIndex = 6;
-            this.label1Example.Text = "Source";
-            // 
-            // tb_TextSourceExample
-            // 
-            this.tb_TextSourceExample.Location = new System.Drawing.Point(646, 210);
-            this.tb_TextSourceExample.Name = "tb_TextSourceExample";
-            this.tb_TextSourceExample.Size = new System.Drawing.Size(100, 22);
-            this.tb_TextSourceExample.TabIndex = 7;
-            this.tb_TextSourceExample.Text = "Hello";
-            // 
-            // button_EncryptTextExample
-            // 
-            this.button_EncryptTextExample.Location = new System.Drawing.Point(584, 245);
-            this.button_EncryptTextExample.Name = "button_EncryptTextExample";
-            this.button_EncryptTextExample.Size = new System.Drawing.Size(75, 23);
-            this.button_EncryptTextExample.TabIndex = 8;
-            this.button_EncryptTextExample.Text = "Encrypt";
-            this.button_EncryptTextExample.UseVisualStyleBackColor = true;
-            this.button_EncryptTextExample.Click += new System.EventHandler(this.button_EncryptTextExample_Click);
-            // 
-            // tB_EncryptExample
-            // 
-            this.tB_EncryptExample.Location = new System.Drawing.Point(665, 245);
-            this.tB_EncryptExample.Name = "tB_EncryptExample";
-            this.tB_EncryptExample.Size = new System.Drawing.Size(97, 22);
-            this.tB_EncryptExample.TabIndex = 9;
-            // 
-            // button_DecryptTextExample
-            // 
-            this.button_DecryptTextExample.Location = new System.Drawing.Point(584, 276);
-            this.button_DecryptTextExample.Name = "button_DecryptTextExample";
-            this.button_DecryptTextExample.Size = new System.Drawing.Size(75, 23);
-            this.button_DecryptTextExample.TabIndex = 10;
-            this.button_DecryptTextExample.Text = "Decrypt";
-            this.button_DecryptTextExample.UseVisualStyleBackColor = true;
-            this.button_DecryptTextExample.Click += new System.EventHandler(this.button_DecryptTextExample_Click);
-            // 
-            // tB_DecryptExample
-            // 
-            this.tB_DecryptExample.Location = new System.Drawing.Point(665, 276);
-            this.tB_DecryptExample.Name = "tB_DecryptExample";
-            this.tB_DecryptExample.Size = new System.Drawing.Size(100, 22);
-            this.tB_DecryptExample.TabIndex = 11;
-            // 
             // button_EncryptFile
             // 
-            this.button_EncryptFile.Location = new System.Drawing.Point(565, 75);
+            this.button_EncryptFile.Location = new System.Drawing.Point(270, 38);
             this.button_EncryptFile.Name = "button_EncryptFile";
             this.button_EncryptFile.Size = new System.Drawing.Size(75, 23);
             this.button_EncryptFile.TabIndex = 12;
             this.button_EncryptFile.Text = "Encrypt File";
             this.button_EncryptFile.UseVisualStyleBackColor = true;
             this.button_EncryptFile.Click += new System.EventHandler(this.button_EncryptFile_Click);
-            // 
-            // button_DecryptFileExample
-            // 
-            this.button_DecryptFileExample.Location = new System.Drawing.Point(584, 305);
-            this.button_DecryptFileExample.Name = "button_DecryptFileExample";
-            this.button_DecryptFileExample.Size = new System.Drawing.Size(75, 23);
-            this.button_DecryptFileExample.TabIndex = 13;
-            this.button_DecryptFileExample.Text = "Decrypt File";
-            this.button_DecryptFileExample.UseVisualStyleBackColor = true;
-            this.button_DecryptFileExample.Click += new System.EventHandler(this.button_DecryptFileExample_Click);
             // 
             // button_DecryptAndPlay
             // 
@@ -157,14 +89,7 @@
             this.ClientSize = new System.Drawing.Size(1058, 389);
             this.Controls.Add(this.comboBoxForCourse);
             this.Controls.Add(this.button_DecryptAndPlay);
-            this.Controls.Add(this.button_DecryptFileExample);
             this.Controls.Add(this.button_EncryptFile);
-            this.Controls.Add(this.tB_DecryptExample);
-            this.Controls.Add(this.button_DecryptTextExample);
-            this.Controls.Add(this.tB_EncryptExample);
-            this.Controls.Add(this.button_EncryptTextExample);
-            this.Controls.Add(this.tb_TextSourceExample);
-            this.Controls.Add(this.label1Example);
             this.Controls.Add(this.tB_FileName);
             this.Controls.Add(this.axWindowsMediaPlayer1);
             this.Name = "Form1";
@@ -179,16 +104,15 @@
 
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
         private System.Windows.Forms.TextBox tB_FileName;
-        private System.Windows.Forms.Label label1Example;
-        private System.Windows.Forms.TextBox tb_TextSourceExample;
-        private System.Windows.Forms.Button button_EncryptTextExample;
-        private System.Windows.Forms.TextBox tB_EncryptExample;
-        private System.Windows.Forms.Button button_DecryptTextExample;
-        private System.Windows.Forms.TextBox tB_DecryptExample;
         private System.Windows.Forms.Button button_EncryptFile;
-        private System.Windows.Forms.Button button_DecryptFileExample;
         private System.Windows.Forms.Button button_DecryptAndPlay;
         private System.Windows.Forms.ComboBox comboBoxForCourse;
+
+
+      
     }
+
+   
+    
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -42,16 +42,19 @@ namespace WindowsFormsApp1
         }
         #endregion Initial UI Data
 
+        string FilePath = System.IO.Path.GetDirectoryName(System.IO.Path.GetDirectoryName(System.Environment.CurrentDirectory)) + "\\File";
 
         private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
         {
+            
         }
 
         private void button_EncryptFile_Click(object sender, EventArgs e)
         {
-            string sourceFilePath = string.Format(@"C:\Testing\LearningSystem\{0}.mp4", tB_FileName.Text);
-            string encryptFilePath = string.Format(@"C:\Testing\LearningSystem\{0}-encrypt.mp4", tB_FileName.Text);
-
+            
+            string sourceFilePath = string.Format("{0}\\{1}.mp4" ,  FilePath, comboBoxForCourse.Text);
+            string encryptFilePath = string.Format("{0}\\{1}-encrypt.mp4", FilePath, comboBoxForCourse.Text);
+            
             using (FileStream sourceStream = new FileStream(sourceFilePath, FileMode.Open, FileAccess.Read))
             using (FileStream encryptStream = new FileStream(encryptFilePath, FileMode.Create, FileAccess.Write))
             {
@@ -69,9 +72,9 @@ namespace WindowsFormsApp1
 
         private void button_DecryptAndPlay_Click(object sender, EventArgs e)
         {
-            string encryptFilePath = string.Format(@"C:\Testing\LearningSystem\{0}-encrypt.mp4", comboBoxForCourse.Text);
-            string decryptFilePath = @"C:\Testing\LearningSystem\Decrypt.mp4";
-
+            string encryptFilePath = string.Format(@"{0}\\{1}-encrypt.mp4", FilePath, comboBoxForCourse.Text);
+            string decryptFilePath = string.Format(@"{0}\\{1}-decrypt.mp4", FilePath, comboBoxForCourse.Text);
+           
             using (FileStream encryptStream = new FileStream(encryptFilePath, FileMode.Open, FileAccess.Read))
             using (FileStream decryptStream = new FileStream(decryptFilePath, FileMode.Create, FileAccess.Write))
             {
@@ -86,88 +89,6 @@ namespace WindowsFormsApp1
 
             axWindowsMediaPlayer1.URL = decryptFilePath;
         }
-
-   
-       
-
-
-        #region example
-        private void button_EncryptTextExample_Click(object sender, EventArgs e)
-        {
-            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
-            byte[] key = Encoding.ASCII.GetBytes("12345678");
-            byte[] iv = Encoding.ASCII.GetBytes("87654321");
-            des.Key = key;
-            des.IV = iv;
-
-            string source = tb_TextSourceExample.Text;
-            byte[] dataByteArray = Encoding.UTF8.GetBytes(source);
-            string encrypt = "";
-
-            using (MemoryStream ms = new MemoryStream())
-            {
-                using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write))
-                {
-                    cs.Write(dataByteArray, 0, dataByteArray.Length);
-                    cs.FlushFinalBlock();
-                    encrypt = Convert.ToBase64String(ms.ToArray());
-                }
-            }
-
-            tB_EncryptExample.Text = encrypt;
-        }
-
-        private void button_DecryptTextExample_Click(object sender, EventArgs e)
-        {
-            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
-            byte[] key = Encoding.ASCII.GetBytes("12345678");
-            byte[] iv = Encoding.ASCII.GetBytes("87654321");
-            des.Key = key;
-            des.IV = iv;
-
-            string encrypt = tB_EncryptExample.Text;
-            string decrypt = "";
-
-            byte[] dataByteArray = Convert.FromBase64String(encrypt);
-
-            using (MemoryStream ms = new MemoryStream())
-            {
-                using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write))
-                {
-                    cs.Write(dataByteArray, 0, dataByteArray.Length);
-                    cs.FlushFinalBlock();
-                    decrypt = Encoding.UTF8.GetString(ms.ToArray());
-                }
-
-                tB_DecryptExample.Text = decrypt;
-            }
-        }
-
-        private void button_DecryptFileExample_Click(object sender, EventArgs e)
-        {
-            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
-            byte[] key = Encoding.ASCII.GetBytes("12345678");
-            byte[] iv = Encoding.ASCII.GetBytes("87654321");
-
-            des.Key = key;
-            des.IV = iv;
-
-            string encryptFilePath = @"C:\Testing\LearningSystem\1-1-encrypt.mp4";
-            string decryptFilePath = @"C:\Testing\LearningSystem\1-1-decrypt.mp4";
-
-            using (FileStream encryptStream = new FileStream(encryptFilePath, FileMode.Open, FileAccess.Read))
-            using (FileStream decryptStream = new FileStream(decryptFilePath, FileMode.Create, FileAccess.Write))
-            {
-                byte[] dataByteArray = new byte[encryptStream.Length];
-                encryptStream.Read(dataByteArray, 0, dataByteArray.Length);
-                using (CryptoStream cs = new CryptoStream(decryptStream, des.CreateDecryptor(), CryptoStreamMode.Write))
-                {
-                    cs.Write(dataByteArray, 0, dataByteArray.Length);
-                    cs.FlushFinalBlock();
-                }
-            }
-        }
-        #endregion example
-
+        
     }
 }
